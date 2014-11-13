0b066883c1931b2406a929534f2003c3bb971da0
Andy <leduigs@users.noreply.github.com>
2014-11-13 17:10:59 +0000

    Update JSONParser.java
    This reads in the first method instead, as we have only one key hostExtended it then creates a JSON array of JSON objects which we can get the information using each of the 4 keys required

--- Project/src/parser/JSONParser.java
+++ Project/src/parser/JSONParser.java
@@ -2,27 +2,20 @@ package parser;
 import org.json.*;
 
 public class JSONParser {
-	
-
-
-<<<<<<< Updated upstream
-	public static String parseJson(String wholeJSONList, String id, String element){
-=======
-	public static String parseJson(String wholeJSONList, String id, String element) throws JSONException{
->>>>>>> Stashed changes
+	public static JSONArray parseJson(String wholeJSONList, String key){
+		key = "hostExtended";
 		JSONObject obj = new JSONObject(wholeJSONList);
-		String pageName = obj.getJSONObject(id).getString(element);
-		
-		return pageName;
+		JSONArray array = obj.getJSONArray(key);
+		return array;		
 	}
 	
 	
+	public static String parseJson(String wholeJSONList, String key, String element) throws JSONException{
+		JSONObject obj = new JSONObject(wholeJSONList);
+		obj = obj.getJSONArray(key).getJSONObject(3);
+		String rack = obj.getString(element);
+		return rack;
+	}
 }
-
-<<<<<<< Updated upstream
 	
-
-
-=======
 	
->>>>>>> Stashed changes
