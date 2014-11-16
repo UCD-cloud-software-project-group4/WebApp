3de10dcc58d4bb99cb0df574a79cbca3bcb531dc
jrcody38 <jonathancody@Jonathans-MacBook-Pro.local>
2014-11-16 00:43:28 +0000

    Add public keywords to ConnectionResults class

--- Project/src/Connections/ConnectionResults.java
+++ Project/src/Connections/ConnectionResults.java
@@ -12,23 +12,23 @@ public class ConnectionResults{
 	
 	
 	
-	ConnectionResults(){
+	public ConnectionResults(){
 		
 	}
 	
-	ConnectionResults(int new_dc_id, int new_floor_id){
+	public ConnectionResults(int new_dc_id, int new_floor_id){
 		dc_id = new_dc_id;
 		floor_id = new_floor_id;
 	}
 	
-	ConnectionResults(int new_dc_id, int new_floor_id, int new_rack_id){
+	public ConnectionResults(int new_dc_id, int new_floor_id, int new_rack_id){
 		dc_id = new_dc_id;
 		floor_id = new_floor_id;
 		rack_id = new_rack_id;
 	}
 	
 	//To be used if looking to create a results for a given Host/server
-	ConnectionResults(int new_dc_id, int new_floor_id, int new_rack_id, int new_host_id){
+	public ConnectionResults(int new_dc_id, int new_floor_id, int new_rack_id, int new_host_id){
 		dc_id = new_dc_id;
 		floor_id = new_floor_id;
 		rack_id = new_rack_id;
