c88bac3cde49e7a9ccafd32491c703cffb68d9ff
Andy <leduigs@users.noreply.github.com>
2014-11-19 14:10:25 +0000

    Update JSONParser.java

--- Project/src/parser/JSONParser.java
+++ Project/src/parser/JSONParser.java
@@ -3,14 +3,17 @@ import org.json.*;
 
 public class JSONParser {
 	public static JSONArray parseJson(String wholeJSONList, String key){
-		key = "hostExtended";
 		JSONObject obj = new JSONObject(wholeJSONList);
 		JSONArray array = obj.getJSONArray(key);
 		return array;		
-
-
 	}
 	
+	//Not being used yet
+	public static JSONObject parseJsonObject(String wholeJSONList, String key){
+		JSONObject obj = new JSONObject(wholeJSONList);
+		JSONObject obj2= obj.getJSONObject(key);
+		return obj2;		
+	}
 	
 	public static String parseJson(String wholeJSONList, String key, String element) throws JSONException{
 		JSONObject obj = new JSONObject(wholeJSONList);
@@ -18,10 +21,6 @@ public class JSONParser {
 		String rack = obj.getString(element);
 		return rack;
 	}
-
 }
 	
 	
-
-
-
